6883b7148f1774eaf8ec8e60b9a97fb689c5ef33
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-09-01 18:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.onclick = getUrlData;
+
+function getUrlData(){
+	
+	output.innerHTML = "";
+	
+	let prom1 = fetch(images[0].url);
+    let prom2 = fetch(images[1].url);
+    let prom3 = fetch(images[2].url);
+
+	let result = Promise.all([prom1,prom2,prom3]);
+	
+	    result.then((data)=>{
+			//data going to be array of images with wich each promise got resolve
+			data.forEach((image)=>{
+				const imageTag = document.createElement("img");
+				imageTag.className = "photos";
+				imageTag.src= `${image.url}`;
+			output.append(imageTag);	
+			});
+			
+		})
+	    .catch((error)=>{
+		     console.log(`Failed to load image's URL: ${error}`);
+	})
+}
